7a562da5a7b5b8da6fd49593ac7c0e86e5a94c7a
cetmolnia <aifedoseev00@gmail.com>
2023-05-11 16:51:18 +0400

    анонимная функция в auth login

--- app/prefix/auth.php
+++ app/prefix/auth.php
@@ -1,6 +1,8 @@
 <?php 
 namespace app\prefix;
 use system\core\user\auth as login;
+use system\core\logs\logs;
+use system\core\request\request;
 use system\core\config\config;
 
 class auth
@@ -9,11 +11,40 @@ class auth
     {
         $authDir = config::globals('authDir');
         if(isset($_POST['email']) && isset($_POST['password'])  && !user_id()){
-            login::redirectFailed($authDir)->redirectSuccess('/')->login();
+            login::redirectFailed($authDir)->redirectSuccess('/')->login(null, null, function($auth, $user, $valid){
+                $logs = [
+                    'ip' => request::get('global')->ip,
+                    'browser_family' => $_SESSION['browser_family'],
+                    'device_name' => $_SESSION['device_name'],
+                    'device_type' => $_SESSION['device_type'],
+                    'os_name' => $_SESSION['os_name'],
+                ];
+                $text = '<ul>
+                    <li>ip - ' . $logs['ip'] . '</li>
+                    <li>browser_family - ' . $logs['browser_family'] . '</li>
+                    <li>device_type - ' . $logs['device_type'] . '</li>
+                    <li>os_name - ' . $logs['os_name'] . '</li>
+                </ul>';
+                logs::userId($user->id)->name('auth', 'Авторизация пользователя')->description($text)->insert($logs, 'auth');
+
+                if($auth->status){
+                    redirect(referal_url());
+                } else {
+                    $authDir = config::globals('authDir');
+                    $error = [
+                        'email' => 'Введённые данные не верны!',
+                    ];
+                    alert2('Войти не удалось!', 'danger', '');
+                    redirect($authDir ?? referal_url(), $valid->data(), $error);
+                }
+            });
         }
 
         if(isset($_REQUEST['output']) && user_id()){
-            login::out();
+            login::out(function(){
+                logs::name('exit', 'Выход пользователя')->insert();
+                header('Location: /');
+            });
         }
     }
 }
